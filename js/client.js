cd0b18b51e353e912a58a7ed879518275c72e4c3
Ashim-Stha <ashimshrestha2384@gmail.com>
2023-12-15 22:34:46 +0545

    left

--- js/client.js
+++ js/client.js
@@ -3,6 +3,8 @@ const socket = io("http://localhost:8000");
 const namee = prompt("Enter your name to join");
 
 const messageContainer = document.querySelector(".container");
+const form = document.getElementById("send-container");
+const messageInput = document.getElementById("messageInp");
 
 socket.emit("new-user-joined", namee);
 
@@ -14,6 +16,22 @@ const append = (message, position) => {
   messageContainer.append(messageElement);
 };
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  append(`You: ${message}`, "right");
+  socket.emit("send", message);
+  messageInput.value = "";
+});
+
 socket.on("user-joined", (name) => {
   append(`${name} joined chat`, "right");
+});
+
+socket.on("receive", (data) => {
+  append(`${data.name}:${data.message}`, "left");
+});
+
+socket.on("left", (name) => {
+  append(`${name} left the chat`, "left");
 });
