393e9de4cbb4872118f8dd5fe076c1da2a38eaad
Ashim-Stha <ashimshrestha2384@gmail.com>
2023-12-15 21:48:22 +0545

    append

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-
-// const app = express();
-
-// const corsOptions = {
-//   origin: "http://127.0.0.1:5500", // Replace with the actual origin of your frontend app
-//   credentials: true, // Allow credentials (cookies, HTTP authentication, etc.)
-// };
-
-// app.use(cors(corsOptions));
-
-// // ... (other middleware and route handling)
-
-// const server = app.listen(8000, () => {
-//   console.log("Server is running on port 8000");
-// });
-
-// const io = require("socket.io")(server);
-
-// ... (Socket.IO configuration)
 const io = require("socket.io")(8000, {
   cors: {
     origin: "http://127.0.0.1:5500",
--- js/client.js
+++ js/client.js
@@ -2,8 +2,18 @@ const socket = io("http://localhost:8000");
 
 const namee = prompt("Enter your name to join");
 
+const messageContainer = document.querySelector(".container");
+
 socket.emit("new-user-joined", namee);
 
-// socket.on("user-joined", (name) => {
-//   console.log(name + " has joined");
-// });
+const append = (message, position) => {
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+  messageContainer.append(messageElement);
+};
+
+socket.on("user-joined", (name) => {
+  append(`${name} joined chat`, "right");
+});
